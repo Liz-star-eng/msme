82e2c05ea9c4be968eabe2fcfcaf24e9555d3b7e
Elizabeth Afriyie <elizabethaf@aiti-kace.com.gh>
2022-05-09 16:24:07 +0000

    errors fixed

--- JS/index.js
+++ JS/index.js
@@ -1,36 +1,17 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "16%";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
 let sidebar = document.getElementById("mySidenav");
 var body = document.getElementById("main-body");
 var span = document.getElementById("span");
 
-function collapsible() {
-  if ((sidebar.style.width = "16%")) {
-    body.style.marginRight = "16%";
-    span.innerHTML = "Hide Side panel >>>"
-  }  else {
-      body.style.marginight = "0";
-      span.innerHTML = "Show Right side<<<"
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "16%";
+  body.style.marginRight = "16%";
+  span.innerHTML = "Hide Side panel >>>";
 }
 
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  body.style.marginRight = "0";
+  span.innerHTML = "Show Right Panel<<<";
+}
 
-//DROPDOWN
-// const content = document.getElementById("a")
-// a.style.backgroundColor = 'white';
-// tippy("#dropdown", {
-//   content: a.innerHTML,
-//   allowHTML: true,
-//   placement: "bottom",
-//   animation: "fade",
-//   interactive: true,
-//  theme: 'light',
-// });
-
-  // collapsible();
+collapsible();
